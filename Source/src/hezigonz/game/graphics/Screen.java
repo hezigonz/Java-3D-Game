91a449e837bd2b466a98838f7e3d189005a39ed6
Hezigonz <sadonkovkiro@gmail.com>
2019-08-09 10:08:45 +0300

    Made the screen pink (idk why lol)

--- Source/src/hezigonz/game/graphics/Screen.java
+++ Source/src/hezigonz/game/graphics/Screen.java
@@ -7,7 +7,14 @@ public class Screen {
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
-		
 		pixels = new int[width * height];
+	}
+	
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff; // Reminder // Hex values start with a 0x //
+			}
+		}
 	}
 }
